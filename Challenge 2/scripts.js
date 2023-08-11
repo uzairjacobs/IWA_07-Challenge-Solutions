473e3edc8647d129cb7fc7d6d8ddf5944be68c01
uzairjacobs <93554483+uzairjacobs@users.noreply.github.com>
2023-08-11 11:38:19 +0200

    completed challenge 2

---
+++ Challenge 2/scripts.js
@@ -0,0 +1,11 @@
+const nickname= "Timmy";
+const firstname = "Timothy";
+
+if (nickname) {
+    console.log("Good morning, ${nickname}!")
+}
+else if(firstname) {
+    console.log("Good morning, ${firstname}!")
+}
+else {"Good morning!"}
+
